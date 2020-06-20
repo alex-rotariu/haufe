fdbf9abbfa74a4b1c87d414c22dbb14acf1e5c64
Alex Rotariu <49029073+alex-rotariu@users.noreply.github.com>
2020-06-20 14:09:57 +0300

    Updates

---
+++ precache-manifest.8b537d4085daf28e73f21dc061b7bf0b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e37ed4a60cf5ff75e44cd840c455e5f",
+    "url": "/haufe/index.html"
+  },
+  {
+    "revision": "e161cf8e2245e5d12e5b",
+    "url": "/haufe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b6ccccba4edcc0ee82bc",
+    "url": "/haufe/static/css/main.1140b31a.chunk.css"
+  },
+  {
+    "revision": "e161cf8e2245e5d12e5b",
+    "url": "/haufe/static/js/2.b209f327.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/haufe/static/js/2.b209f327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ccccba4edcc0ee82bc",
+    "url": "/haufe/static/js/main.dd9e1e02.chunk.js"
+  },
+  {
+    "revision": "222b687819d4ae57dcb8",
+    "url": "/haufe/static/js/runtime-main.49a4e5a4.js"
+  },
+  {
+    "revision": "a155a92a9e6071380cc49ce29123f422",
+    "url": "/haufe/static/media/CV.a155a92a.pdf"
+  },
+  {
+    "revision": "804e302053db2bcebf1b9c83c5a34985",
+    "url": "/haufe/static/media/StanCasa1.804e3020.jpg"
+  },
+  {
+    "revision": "7becf56208a1a42ab9a4b9e21d3ac08e",
+    "url": "/haufe/static/media/StanCasa2.7becf562.jpg"
+  },
+  {
+    "revision": "90901f11e5b9e093b55a8952efb5bc1e",
+    "url": "/haufe/static/media/myPhoto.90901f11.jpg"
+  }
+]);
